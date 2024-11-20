0bd31f46f43074d68a6cdce229462df857cf0e32
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-20 16:32:49 -0300

    feat: adiciona opção de selecionar imagem

--- src/app/shared/components/figure/figure.component.ts
+++ src/app/shared/components/figure/figure.component.ts
@@ -6,9 +6,24 @@ import { Component, Input } from '@angular/core';
   styleUrl: './figure.component.scss'
 })
 export class FigureComponent {
+  selectedFile: File | null = null;
+
   @Input() rotate: number = 0;
   @Input() width: number = 134;
   @Input() height: number = 188;
   @Input() text: string = '';
   @Input() arrow: 'up' | 'down' = 'down';
+
+  triggerFileInput(fileInput: HTMLInputElement): void {
+    fileInput.click();
+  }
+
+  onFileSelected(event: Event, figureEl: HTMLElement): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      figureEl.style.backgroundImage = `url(${URL.createObjectURL(this.selectedFile)})`;
+    }
+  }
 }
