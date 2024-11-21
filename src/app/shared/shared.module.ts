ee7172ffd06bc44ab0f87c03a1b5f7a94fec8c8c
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-21 19:06:25 -0300

    feat: adiciona diretiva para destaque de figurinhas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LeftPageComponent } from './components/left-page/left-page.component';
 import { FigureComponent } from './components/figure/figure.component';
 import { FillableStickerComponent } from './components/fillable-sticker/fillable-sticker.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     LeftPageComponent,
     FigureComponent,
     FillableStickerComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
@@ -17,6 +19,7 @@ import { FillableStickerComponent } from './components/fillable-sticker/fillable
     LeftPageComponent,
     FigureComponent,
     FillableStickerComponent,
+    HighlightDirective,
   ],
 })
 export class SharedModule { }
