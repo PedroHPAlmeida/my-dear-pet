4d455214771e81596b8b9bb0d1ab9b0d334167f5
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-21 19:59:39 -0300

    fix: corrige diretiva de highlight

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,10 +1,36 @@
-import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: '[appHighlight]',
 })
 export class HighlightDirective {
+  _rotate: number = 0;
+
+  @Input()
+  set rotate(value: number) {
+    this._rotate = value;
+    this.updateTransform();
+  }
+
+  transition: string = 'transform 0.3s ease-in-out';
+  transform: string = '';
+  transformHover: string = `rotate(0deg) scale(1.25)`;
+
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    this.renderer.addClass(this.el.nativeElement, 'highlight');
+    this.renderer.setStyle(this.el.nativeElement, 'transition', this.transition);
+    this.updateTransform();
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.renderer.setStyle(this.el.nativeElement, 'transform', this.transformHover);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.renderer.setStyle(this.el.nativeElement, 'transform', this.transform);
+  }
+
+  private updateTransform() {
+    this.transform = `rotate(${this._rotate}deg) scale(1)`;
+    this.renderer.setStyle(this.el.nativeElement, 'transform', this.transform);
   }
 }
