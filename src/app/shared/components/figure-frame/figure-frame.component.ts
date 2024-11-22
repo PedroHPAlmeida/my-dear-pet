add3a04e606f8fa382eceac3a58cf15f368786b2
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-11-21 22:06:30 -0300

    feat: adiciona borda de imagem

---
+++ src/app/shared/components/figure-frame/figure-frame.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-figure-frame',
+  templateUrl: './figure-frame.component.html',
+  styleUrl: './figure-frame.component.scss'
+})
+export class FigureFrameComponent {
+
+}
