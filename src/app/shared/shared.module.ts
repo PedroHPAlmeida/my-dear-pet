c2d0b68a9e29776b07d759b21fd217bf51ce0481
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-12-05 20:43:47 -0300

    feat: adiciona componente flipbook

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,9 @@ import { LeftPageComponent } from './components/left-page/left-page.component';
 import { FigureComponent } from './components/figure/figure.component';
 import { FillableStickerComponent } from './components/fillable-sticker/fillable-sticker.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { BookComponent } from './components/flipbook/book/book.component';
+import { PageComponent } from './components/flipbook/page/page.component';
+import { ReversePipe } from './components/flipbook/pipes/reverse.pipe';
 
 @NgModule({
   declarations: [
@@ -11,6 +14,9 @@ import { HighlightDirective } from './directives/highlight.directive';
     FigureComponent,
     FillableStickerComponent,
     HighlightDirective,
+    BookComponent,
+    PageComponent,
+    ReversePipe,
   ],
   imports: [
     BrowserModule,
@@ -20,6 +26,7 @@ import { HighlightDirective } from './directives/highlight.directive';
     FigureComponent,
     FillableStickerComponent,
     HighlightDirective,
+    BookComponent,
   ],
 })
 export class SharedModule { }
